1155263e723ff17c58e7b08662e80b0cd05a67ba
radule90 <>
2023-06-20 07:51:47 +0000

    Test deploy

--- bag/contexts.py
+++ bag/contexts.py
@@ -5,10 +5,12 @@ from products.models import Product
 
 
 def bag_contents(request):
+
     bag_items = []
     total = 0
     product_count = 0
     bag = request.session.get('bag', {})
+
     for item_id, item_data in bag.items():
         if isinstance(item_data, int):
             product = get_object_or_404(Product, pk=item_id)
@@ -17,7 +19,7 @@ def bag_contents(request):
             bag_items.append({
                 'item_id': item_id,
                 'quantity': item_data,
-                'product': product
+                'product': product,
             })
         else:
             product = get_object_or_404(Product, pk=item_id)
@@ -28,22 +30,25 @@ def bag_contents(request):
                     'item_id': item_id,
                     'quantity': quantity,
                     'product': product,
-                    'size': size
+                    'size': size,
                 })
+
     if total < settings.FREE_DELIVERY_THRESHOLD:
-        delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE)
+        delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
         free_delivery_delta = settings.FREE_DELIVERY_THRESHOLD - total
     else:
         delivery = 0
         free_delivery_delta = 0
+
     grand_total = delivery + total
+
     context = {
         'bag_items': bag_items,
         'total': total,
         'product_count': product_count,
         'delivery': delivery,
         'free_delivery_delta': free_delivery_delta,
-        'free_deliverey_threshold': settings.FREE_DELIVERY_THRESHOLD,
+        'free_delivery_threshold': settings.FREE_DELIVERY_THRESHOLD,
         'grand_total': grand_total,
     }
 
