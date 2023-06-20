b63603072fb66fee7ca76f47fcf0f0c754c0035c
radule90 <>
2023-06-20 10:40:49 +0000

    test

--- products/admin.py
+++ products/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from .models import Product, Category
-# Register your models here.
 
+# Register your models here.
 
 class ProductAdmin(admin.ModelAdmin):
     list_display = (
@@ -12,15 +12,14 @@ class ProductAdmin(admin.ModelAdmin):
         'rating',
         'image',
     )
-    ordering = ('sku',)
 
+    ordering = ('sku',)
 
 class CategoryAdmin(admin.ModelAdmin):
     list_display = (
         'friendly_name',
         'name',
     )
-
 
 admin.site.register(Product, ProductAdmin)
 admin.site.register(Category, CategoryAdmin)
--- products/urls.py
+++ products/urls.py
@@ -1,5 +1,3 @@
-from django.contrib import admin
-
 from django.urls import path
 from . import views
 
@@ -9,5 +7,4 @@ urlpatterns = [
     path('add/', views.add_product, name='add_product'),
     path('edit/<int:product_id>/', views.edit_product, name='edit_product'),
     path('delete/<int:product_id>/', views.delete_product, name='delete_product'),
-
 ]
